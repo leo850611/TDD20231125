08702a51b2611c5adcae24abf54e3cd4e3cb2562
Leo <leo850611@hotmail.com>
2023-11-26 00:04:41 +0800

    refactor

--- TddBudget/TddBudget/Service/BudgetService.cs
+++ TddBudget/TddBudget/Service/BudgetService.cs
@@ -1,4 +1,5 @@
 using TddBudget.Interface;
+using TddBudget.Model;
 
 namespace TddBudget.Service;
 
@@ -14,40 +15,27 @@ public class BudgetService
     public decimal Query(DateTime start, DateTime end)
     {
         var budgets = _budgetRepo.GetAll();
-        if (start.ToString("yyyyMM") != end.ToString("yyyyMM"))
-        {
-            var totalAmount = 0m;
-            for (var i = start.Month; i <= end.Month; i++)
-            {
-                if (i == start.Month)
-                {
-                    var budget = budgets.First(x => x.YearMonth == $"{start.Year}{start.Month:00}");
-                    var totalDays = (decimal) (new DateTime(2023, start.Month, 31) - start).TotalDays + 1;
-                    var oneDayAmount = budget.GetOneDayAmount();
-                    totalAmount += totalDays * oneDayAmount;
-                }
-                else if (i == end.Month)
-                {
-                    var budget2 = budgets.First(x => x.YearMonth == $"{end.Year}{end.Month:00}");
-                    var totalDays2 = (decimal) (end - new DateTime(2023, end.Month, 1)).TotalDays + 1;
-                    var oneDayAmount2 = budget2.GetOneDayAmount();
-                    totalAmount += totalDays2 * oneDayAmount2;
-                }
-                else
-                {
-                    var budget2 = budgets.First(x => x.YearMonth == $"{start.Year}{i:00}");
-                    totalAmount += budget2.Amount;
-                }
-            }
+        var totalAmount = 0m;
 
-            return totalAmount;
-        }
-        else
+        var startDate = start;
+        var endDate = end;
+        while (startDate <= endDate)
         {
-            var budget = budgets.First(x => x.YearMonth == $"{start.Year}{start.Month:00}");
-            var totalDays = (decimal) (end - start).TotalDays + 1;
-            var oneDayAmount = budget.GetOneDayAmount();
-            return totalDays * oneDayAmount;
+            var lastDateInStartMonth = new DateTime(startDate.Year, startDate.Month,
+                DateTime.DaysInMonth(startDate.Year, startDate.Month));
+            totalAmount += GetAmount(startDate, lastDateInStartMonth < endDate ? lastDateInStartMonth : end, budgets);
+
+            startDate = lastDateInStartMonth.AddDays(1);
         }
+
+        return totalAmount;
+    }
+
+    private decimal GetAmount(DateTime start, DateTime end, IEnumerable<Budget> budgets)
+    {
+        var budget = budgets.First(x => x.YearMonth == $"{start.Year}{start.Month:00}");
+        var totalDays = (decimal) (end - start).TotalDays + 1;
+        var oneDayAmount = budget.GetOneDayAmount();
+        return totalDays * oneDayAmount;
     }
 }
